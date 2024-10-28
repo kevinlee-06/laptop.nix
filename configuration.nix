4498fd2bd033adf127105921fc6364c5df7977d9
kevinlee-06 <cgshyuankai@proton.me>
2024-10-28 11:15:31 +0800

    First commit, Nvidia is working

---
+++ configuration.nix
@@ -0,0 +1,247 @@
+{ config, pkgs, lib, ... }:
+
+# Nix Software Center
+let
+  nix-software-center = import (pkgs.fetchFromGitHub {
+    owner = "snowfallorg";
+    repo = "nix-software-center";
+    rev = "0.1.2";
+    sha256 = "xiqF1mP8wFubdsAQ1BmfjzCgOD3YZf7EGWl9i69FTls=";
+  }) {};
+in
+
+{
+  # Hardware config
+  imports = [
+    ./hardware-configuration.nix
+    ./nvidia.nix
+    ./zerotier.nix
+  ];
+
+  # Bootloader
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  # Network
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+    firewall = {
+      enable = false;
+      # allowedTCPPorts = [ ... ];
+      # allowedUDPPorts = [ ... ];
+    };
+  };
+
+  # Time & Locale & Input method
+  time.timeZone = "Asia/Taipei";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "zh_TW.UTF-8";
+      LC_IDENTIFICATION = "zh_TW.UTF-8";
+      LC_MEASUREMENT = "zh_TW.UTF-8";
+      LC_MONETARY = "zh_TW.UTF-8";
+      LC_NAME = "zh_TW.UTF-8";
+      LC_NUMERIC = "zh_TW.UTF-8";
+      LC_PAPER = "zh_TW.UTF-8";
+      LC_TELEPHONE = "zh_TW.UTF-8";
+      LC_TIME = "zh_TW.UTF-8";
+    };
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "zh_TW.UTF-8/UTF-8"
+    ];
+    inputMethod = {
+      # enabled = "fcitx5";
+      # type = "fcitx5";
+      # enable = true;
+      # waylandFrontend = true;
+      fcitx5.addons = with pkgs; [
+        fcitx5-gtk
+        fcitx5-chinese-addons 
+        fcitx5-nord
+        fcitx5-chewing
+     ];
+    };
+  };
+
+  services.xserver = {
+    enable = true;
+    desktopManager.gnome.enable = true;
+    displayManager.gdm = {
+      enable = true;
+      wayland = true;
+    };
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
+  };
+
+  # # Desktop
+  # services.xserver.displayManager.gdm = {
+  #   enable = true;
+  #   wayland = true;
+  # };
+  # services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.desktopManager.cinnamon.enable = true;
+  # X11
+  # services.xserver.enable = true;
+  # services.xserver.xkb = {
+  #   layout = "us";
+  #   variant = "";
+  # };
+
+  # CUPS
+  services.printing.enable = true;
+
+  # Audio
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Users
+  users.users.kevin = {
+    isNormalUser = true;
+    description = "kevin";
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker" ];
+    packages = with pkgs; [
+      thunderbird
+      vscode
+      vesktop
+      localsend
+      remmina
+      brave
+      vlc
+      scrcpy
+      lshw
+    ];
+  };
+
+  users.users.npc = {
+    isNormalUser = true;
+    description = "npc";
+    extraGroups = [ "networkmanager" "docker" ];
+    packages = with pkgs; [
+      thunderbird
+      vscode
+      vesktop
+      chromium
+      brave
+      vlc
+    ];
+  };
+
+  # Firefox
+  programs.firefox.enable = true;
+  
+  # Repo
+  nixpkgs.config.allowUnfree = true;
+  
+  # Flatpak
+  services.flatpak.enable = true;
+
+  # VMM
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
+
+  # SSH
+  services.openssh.enable = true;
+  
+  # ADB
+  programs.adb.enable = true;
+
+  # Packages
+  environment.systemPackages = with pkgs; [
+    btop
+    cava
+    curl
+    distrobox
+    git
+    gparted
+    gnome-themes-extra
+    gtk-engine-murrine
+    htop
+    qemu
+    sassc
+    virt-manager
+    neovim
+    neofetch
+    nix-software-center
+    unimatrix
+    vim
+    wget
+    python313Full
+    go
+    nodejs_22
+    glxinfo
+    mesa
+    gcc
+    rustc
+    rustup
+    tmux
+    gnome.gnome-tweaks
+    gnome-extension-manager
+  ];
+
+  # Font packages
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      ubuntu_font_family
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts.githubRelease
+      dina-font
+      proggyfonts
+    ];
+
+    # Display fonts
+    fontconfig = {
+      defaultFonts = {
+        serif = [ "Noto Sans CJK TC" "DejaVu Sans" "Liberation Serif" "Vazirmatn" ];
+        sansSerif = [ "Noto Sans CJK TC" "Ubuntu" "Noto Sans" ];
+        monospace = [ "Source Code Pro" "Ubuntu Mono" ];
+      };
+    };
+  };
+
+  environment.variables.EDITOR = "vim";
+
+ # Docker
+  virtualisation = {
+    podman = {
+      enable = true;
+      dockerCompat = false;
+    };
+    docker = {
+      enable = true;
+      rootless = {
+          enable = true;
+          setSocketVariable = true;
+      };
+    };
+  };
+
+  # Keep alive
+  systemd.targets = {
+    sleep.enable = false;
+    suspend.enable = false;
+    hibernate.enable = false;
+    hybrid-sleep.enable = false; 
+  };
+
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
---
+++ nvidia.nix
@@ -0,0 +1,38 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services = {
+    xserver = {
+      videoDrivers = ["nvidia"];
+    };
+  };
+
+  hardware = {
+    opengl.enable = true;
+    nvidia =  {
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "550.127.05";
+        sha256_64bit = "sha256-04TzT10qiWvXU20962ptlz2AlKOtSFocLuO/UZIIauk=";
+        sha256_aarch64 = "sha256-mVEeFWHOFyhl3TGx1xy5EhnIS/nRMooQ3+LdyGe69TQ=";
+        openSha256 = "sha256-Po+pASZdBaNDeu5h8sgYgP9YyFAm9ywf/8iyyAaLm+w=";
+        settingsSha256 = "sha256-cUSOTsueqkqYq3Z4/KEnLpTJAryML4Tk7jco/ONsvyg=";
+        persistencedSha256 = "sha256-Vz33gNYapQ4++hMqH3zBB4MyjxLxwasvLzUJsCcyY4k=";
+      };
+      modesetting.enable = true;
+      open = false;
+      nvidiaSettings = true;
+      #dynamicBoost.enable = true;
+      #powerManagement.enable = true;
+      powerManagement.finegrained = true;
+      #nvidiaPersistenced = true;
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+  };
+}
